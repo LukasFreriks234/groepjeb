ee9088dfac03f3d06a5114b11ca0c659f46a146f
anniek-dev <am.edelaar@student.avans.nl>
2026-04-15 19:58:52 +0200

    add grid to db

---
+++ app/Http/Controllers/GridCell.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\GridCell;
+
+class GridCell extends Controller
+{
+    public function index() {
+        $cells = GridCell::orderBy('y_coordinate')->orderBy('x_coordinate')->get();
+        return view('welcome', compact('cells'));
+    }
+}
---
+++ app/Models/GridCell.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class GridCell extends Model
+{
+    protected $fillable = ['x_coordinate', 'y_coordinate', 'is_available', 'destination_type'];
+}
