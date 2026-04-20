66c9c95b93c75e03c9f7da5899dd4cd56ce3b3f6
DionM02 <dionm02@outlook.com>
2026-04-20 15:41:35 +0200

    beter

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,17 +3,39 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Voor Dion: het javascript en css staan in public in hun respectieve mapjes-->
     <script type="text/javascript" src="{{ asset('js/functionTable.js') }}" defer></script>
+    <script src="{{ asset('js/gridDragDrop.js') }}" defer></script>
     <link href="{{ asset('css/styles.css')}}" type="text/css" rel="stylesheet"/>
+    <link href="{{ asset('css/griddion.css')}}" type="text/css" rel="stylesheet"/>
+    <link href="{{ asset('css/grid.css')}}" type="text/css" rel="stylesheet"/>
     <title>Document</title>
 </head>
 <body>
+        <div class="simulation-container">
+    <main class="city-section">
+        <h2>City area</h2>
+        <div class="metropolis-grid" role="grid">
+            @foreach($cells as $cell)
+                <div class="grid-cell {{ $cell->is_available ? 'available' : 'occupied' }}" 
+                     data-id="{{ $cell->id }}"
+                     role="gridcell"
+                     tabindex="0"
+                     aria-label="Cel on position {{ $cell->x_coordinate }}, {{ $cell->y_coordinate }}. Status: {{ $cell->is_available ? 'avilable' : 'occupied' }}">
+                    <!-- @if(!$cell->is_available && $cell->cityFunction)
+                        <img src="{{ asset('storage/' . $cell->cityFunction->image_path) }}" 
+                            alt="{{ $cell->cityFunction->name }}" 
+                            class="grid-image">
+                    @endif --> 
+                </div>
+            @endforeach
+        </div>
+    </main>
+</div>
     <div id="functions_table">
      <input type="text" id="myInput" placeholder="Search for names.."><br>
 
         <input type="checkbox" id="category1" class="function_filter" name="category1" value="Germany">
-        <label for="vcategory1"> Germany</label><br>
+        <label for="category1"> Germany</label><br>
         <input type="checkbox" id="category2" class="function_filter" name="category2" value="Sweden">
         <label for="category2"> Sweden</label><br>
         <input type="checkbox" id="category3" class="function_filter" name="category3" value="UK">
@@ -21,43 +43,45 @@
 
 
             <ul id="functions_list">
-                
-                <li><div class="function_image"><img src=
-                "{{url('/images/photo-masthead-375-BoK_p8LG.webp')}}"
-                ></div>
-                <div><p class="function_name">
-                Alfreds Futterkiste
-                </p><p class="function_category" name="Germany">
-                Germany
-                </p></div></li>
+                <li id="function-1" class="function-item" draggable="true">
+                    <div class="function_image">
+                        <img src="{{ url('/images/photo-masthead-375-BoK_p8LG.webp') }}">
+                    </div>
+                    <div>
+                        <p class="function_name">Alfreds Futterkiste</p>
+                        <p class="function_category">Germany</p>
+                    </div>
+                </li>
 
-                <li><div class="function_image"><img src=
-                "{{url('/images/UtsiktverJrvsfrnStenegrd_CMSTemplate.jpg')}}"
-                ></div>
-                <div><p class="function_name">
-                Berglunds snabbkop
-                </p><p class="function_category" name="Sweden">
-                Sweden
-                </p></div></li>
+                <li id="function-2" class="function-item" draggable="true">
+                    <div class="function_image">
+                        <img src="{{ url('/images/UtsiktverJrvsfrnStenegrd_CMSTemplate.jpg') }}">
+                    </div>
+                    <div>
+                        <p class="function_name">Berglunds snabbkop</p>
+                        <p class="function_category">Sweden</p>
+                    </div>
+                </li>
 
-                <li><div class="function_image"><img src=
-                "{{url('/images/LY-AR6.webp')}}"
-                ></div>
-                <div><p class="function_name">
-                Island Trading
-                </p><p class="function_category" name="UK">
-                UK
-                </p></div></li>
+                <li id="function-3" class="function-item" draggable="true">
+                    <div class="function_image">
+                        <img src="{{ url('/images/LY-AR6.webp') }}">
+                    </div>
+                    <div>
+                        <p class="function_name">Island Trading</p>
+                        <p class="function_category">UK</p>
+                    </div>
+                </li>
 
-                <li><div class="function_image"><img src=
-                "{{url('/images/Essen-Südviertel_Luft.jpg')}}"
-                ></div>
-                <div><p class="function_name">
-                Koniglich Essen
-                </p><p class="function_category" name="Germany">
-                Germany
-                </p></div></li>
-
+                <li id="function-4" class="function-item" draggable="true">
+                    <div class="function_image">
+                        <img src="{{ url('/images/Essen-Südviertel_Luft.jpg') }}">
+                    </div>
+                    <div>
+                        <p class="function_name">Koniglich Essen</p>
+                        <p class="function_category">Germany</p>
+                    </div>
+                </li>
             </ul>
     </div>
 </body>
