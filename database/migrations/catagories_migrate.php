c7a552a5583cc65c8549bd2987ef4f7b58f9b5e5
Lotte <spapenslotte@gmail.com>
2026-04-21 15:33:13 +0200

    small fixes

--- database/migrations/catagories_migrate.php
+++ database/migrations/catagories_migrate.php
@@ -12,8 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('categories', function (Blueprint $table) {
-            $table->id();
-            $table->string('category');
+            $table->string('category')->primary();
         });
     }
 
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,5 +15,6 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(CategoriesSeeder::class);
+        $this->call(GridCellSeeder::class);
     }
 }
