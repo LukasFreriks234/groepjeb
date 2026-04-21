241442af16167e75963ff7a68cb137770ad54b42
WWLimpens <wouter.limpens.2002@gmail.com>
2026-04-21 17:51:51 +0200

    eerste stap components

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,4 @@ use App\Http\Controllers\GridCellController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/test', function() {
-    return "De route werkt!";
-});
-
 Route::get('/', [GridCellController::class, 'index']);
