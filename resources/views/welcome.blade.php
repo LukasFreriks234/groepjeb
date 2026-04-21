ed6a2216cab4f16e511a70b4fc4889f6281a085e
WWLimpens <wouter.limpens.2002@gmail.com>
2026-04-21 18:09:26 +0200

    beter css naamgeving en verbetering fout component referentie

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,14 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script type="text/javascript" src="{{ asset('js/functionTable.js') }}" defer></script>
     <script src="{{ asset('js/gridDragDrop.js') }}" defer></script>
-    <link href="{{ asset('css/styles.css')}}" type="text/css" rel="stylesheet"/>
+    <link href="{{ asset('css/functionTableStyle.css')}}" type="text/css" rel="stylesheet"/>
     <link href="{{ asset('css/griddion.css')}}" type="text/css" rel="stylesheet"/>
-    <link href="{{ asset('css/grid.css')}}" type="text/css" rel="stylesheet"/>
+    <link href="{{ asset('css/gridStyle.css')}}" type="text/css" rel="stylesheet"/>
     <title>Document</title>
 </head>
 <body>
 
-<grid :cells="$cells"/>
+<x-grid :cells="$cells"/>
 <x-functionTable/>
 
 </body>
