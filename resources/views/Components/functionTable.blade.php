6b7a77653eb2ea1116ff51f63b26071859f1b465
WWLimpens <wouter.limpens.2002@gmail.com>
2026-04-22 15:00:58 +0200

    sorted arrays

--- resources/views/Components/functionTable.blade.php
+++ resources/views/Components/functionTable.blade.php
@@ -1,22 +1,31 @@
 <div id="functions_table">
     <input type="text" id="myInput" placeholder="Search for names.."><br>
 
-    <?php $i=1;?>
-    @foreach($categories as $category)
-        <input type="checkbox" id="category{{ $i }}" class="function_filter" name="category{{ $i }}" value="{{ $category->category }}">
-        <label for="category{{ $i }}">{{ $category->category }}</label><br>
+    <?php 
+    $arrCategories = $categories->toArray();
+    $arrCategorie = array_column($arrCategories, 'category');
+    array_multisort($arrCategorie, SORT_ASC, $arrCategories);
+    $i=1;?>
+    @foreach($arrCategories as $category)
+        <input type="checkbox" id="category{{ $i }}" class="function_filter" name="category{{ $i }}" value="{{ $category['category'] }}">
+        <label for="category{{ $i }}">{{ $category['category'] }}</label><br>
         <?php $i++;?>
     @endforeach
 
+        <?php 
+        $arrFunctions = $functions->toArray();
+        $arrFunctionName = array_column($arrFunctions, 'name');
+        $arrFunctionCategory = array_column($arrFunctions, 'category');
+        array_multisort($arrFunctionCategory, SORT_ASC, $arrFunctionName, SORT_ASC,$arrFunctions);?>
         <ul id="functions_list">
-            @foreach($functions as $function)
-                <li id="{{ $function->id }}" class="function-item" draggable="true">
+            @foreach($arrFunctions as $function)
+                <li id="{{ $function['id'] }}" class="function-item" draggable="true">
                 <div class="function_image">
-                    <img src="{{ $function->image }}">
+                    <img src="{{ $function['image'] }}">
                 </div>
                 <div>
-                    <p class="function_name">{{ $function->name }}</p>
-                    <p class="function_category" name="{{ $function->category }}">{{ $function->category }}</p>
+                    <p class="function_name">{{ $function['name'] }}</p>
+                    <p class="function_category" name="{{ $function['category'] }}">{{ $function['category'] }}</p>
                 </div>
             </li>
             @endforeach
