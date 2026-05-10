7a6815fa1c19910cf512f666c8bbf5e8813f759a
anniek-dev <am.edelaar@student.avans.nl>
2026-05-10 21:30:07 +0200

    effect tabel + aanpassingen sprint review

---
+++ resources/views/Components/effectTable.blade.php
@@ -0,0 +1,12 @@
+<div>
+    <h2>Effects</h2>
+    <ul id="effectsList">
+       <li>safety</li>
+       <li>Environmental Quality</li>
+       <li> Mobility</li>
+       <li>recreation</li>
+       <li>Services</li>
+    </ul>
+</div>
+
+<!-- //word dynamisch maar is nu voor de beeld vorming -->
