aca7f4d65807ae12bf1c51365bd2ffd78cda2797
anniek-dev <am.edelaar@student.avans.nl>
2026-05-14 10:41:21 +0200

    dynamisch

--- resources/views/Components/effectTable.blade.php
+++ resources/views/Components/effectTable.blade.php
@@ -1,12 +1,9 @@
 <div>
     <h2>Effects</h2>
     <ul id="effectsList">
-       <li>safety</li>
-       <li>Environmental Quality</li>
-       <li> Mobility</li>
-       <li>recreation</li>
-       <li>Services</li>
+       @foreach ($categories as $category)
+            <li>{{ $category->category}}</li>
+        @endforeach
     </ul>
 </div>
 
-<!-- //word dynamisch maar is nu voor de beeld vorming -->
