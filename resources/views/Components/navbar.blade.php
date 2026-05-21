4e8dd6f32c5b87ceb44381a74878add5fe6af77e
anniek-dev <am.edelaar@student.avans.nl>
2026-05-21 21:19:44 +0200

    wcag

--- resources/views/Components/navbar.blade.php
+++ resources/views/Components/navbar.blade.php
@@ -1,6 +1,6 @@
-<nav class="navbar">
+<nav class="navbar" aria-label="Main Navigation">
         <ul class="navbar-menu">
-            <li class="navbar-item"><a href="/">Home</a></li>
+            
         </ul>
     </div>
 </nav>
