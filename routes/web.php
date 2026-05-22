1a3fa17f37ae940baf97992a0453a84ce2345faf
Mikay Verhees <m.verhees2@student.avans.nl>
2026-05-22 10:24:30 +0200

    Login systeem, de effect totalen en tooltip staan nu wel de hele tijd op 0

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,23 @@
 <?php
 
 use App\Http\Controllers\GridCellController;
+use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [GridCellController::class, 'index']);
+Route::get('/', [SessionController::class, 'create']);
 
-Route::post('/grid/assign-function', [GridCellController::class, 'assignFunction']);
+Route::post('/', [SessionController::class, 'store']);
 
-Route::post('/remove-function', [GridCellController::class, 'removeFunction']);
+Route::post('/logout', [SessionController::class, 'destroy']);
 
-Route::post('/grid/neighbor-effects', [GridCellController::class, 'neighborEffects']);
+Route::middleware(['auth', 'role:cityplanner,admin'])->group(function (){
+
+    Route::get('/grid', [GridCellController::class, 'index']);
+
+    Route::post('/grid/assign-function', [GridCellController::class, 'assignFunction']);
+
+    Route::post('/remove-function', [GridCellController::class, 'removeFunction']);
+
+    Route::post('/grid/neighbor-effects', [GridCellController::class, 'neighborEffects']);
+
+});
