bcec01920c52242eb84db73c6d056c9921190920
LukasFreriks <99197020+LukasFreriks@users.noreply.github.com>
2026-06-03 13:20:51 +0200

    Merge afgerond

--- resources/views/Components/functionTable.blade.php
+++ resources/views/Components/functionTable.blade.php
@@ -1,8 +1,7 @@
 <div id="functionsTable">
     <div class="filters">
         <div>
-            <label for="myInput">Search functions</label>
-            <input type="text" id="myInput" placeholder="Search for names.."><br>
+            <input type="text" id="myInput" placeholder="Search for functions.." aria-label="searchbar for functions"><br>
 
             <?php 
             $arrCategories = $categories->toArray();
@@ -32,13 +31,9 @@
     array_multisort($arrFunctionCategory, SORT_ASC, $arrFunctionName, SORT_ASC, $arrFunctions);
     ?>
 
-    <p id="keyboardDragInstructions" class="sr-only">
-        Keyboard instructions: use Tab or Shift Tab to move through the function list. Press Enter or Space on a function to select it. Then move to a grid cell and press Enter or Space to place it.
-    </p>
-
-    <ul id="functionsList" aria-describedby="keyboardDragInstructions">
+    <ul id="functionsList" tabindex="-1" aria-label="functions" >
         @foreach($arrFunctions as $function)
-            <li 
+            <li tabindex="0"
                 id="function{{ $function['id'] }}" 
                 class="functionItem keyboardDraggableFunction"
                 data-function-id="{{ $function['id'] }}"
@@ -48,15 +43,16 @@
                 aria-label="Select function {{ $function['name'] }} in category {{ $function['category'] }} to place it in the grid. Effects: Safety {{ data_get($function, 'effects.Safety', 0) }}, Recreation {{ data_get($function, 'effects.Recreation', 0) }}, Environmental Quality {{ data_get($function, 'effects.Environmental Quality', 0) }}, Services {{ data_get($function, 'effects.Services', 0) }}, Mobility {{ data_get($function, 'effects.Mobility', 0) }}."
             >
                 <div class="functionImage">
-                    <img 
+                    <img
                         src="{{ asset($function['image']) }}" 
                         alt="{{ $function['name'] }}"
                         data-category="{{ $function['category'] }}"
                     >
                 </div>
 
-                <div>
+                <div class="functionDescription">
                     <p class="functionName">{{ $function['name'] }}</p>
+                    <p class="sr-only">Category</p>
                     <p class="functionCategory" name="{{ $function['category'] }}">
                         {{ $function['category'] }}
                     </p>
