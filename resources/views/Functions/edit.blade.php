d9f416a27e27bbe223d1e6936f080326c609c176
anniek-dev <am.edelaar@student.avans.nl>
2026-06-03 15:20:22 +0200

    back button

--- resources/views/Functions/edit.blade.php
+++ resources/views/Functions/edit.blade.php
@@ -202,12 +202,17 @@
                 value="{{ old('Mobility', data_get($function->effects, 'Mobility', 0)) }}"
                 class="{{ (old('Mobility', data_get($function->effects, 'Mobility', 0)) > 0) ? 'positiveEffect' : ((old('Mobility', data_get($function->effects, 'Mobility', 0)) < 0) ? 'negativeEffect' : 'neutralEffect') }}"
             >
-
+        <div class="button-group">
             <!-- SAVE -->
             <button type="submit">
                 Save changes
             </button>
         </form>
+    </div>
+
+    <a href="{{ route('functions.show', $function->id) }}">
+                <button type="button">Back</button>
+    </a>
     </div>
 
     <!-- IMAGE -->
