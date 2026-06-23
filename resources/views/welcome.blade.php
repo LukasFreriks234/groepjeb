2ae870ed19ae61d5bfbd28181fbd7468e7acd5f0
DionM02 <dionm02@outlook.com>
2026-06-23 22:24:33 +0200

    dynamic

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,6 +11,8 @@
 <script src="{{ asset('js/switchFunctionEvent.js') }}" defer></script>
 <script src="{{ asset('js/mainroads.js') }}" defer></script>
 <script src="{{ asset('js/simulationTimer.js') }}" defer></script>
+<script src="{{ asset('js/dynamicEvent.js') }}" defer></script>
+
 
 <link href="{{ asset('css/functionTableStyle.css') }}" type="text/css" rel="stylesheet"/>
 <link href="{{ asset('css/gridStyle.css') }}" type="text/css" rel="stylesheet"/>
