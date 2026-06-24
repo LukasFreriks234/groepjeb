b00f0ef1d6fa2d022988b37905b986492163eff4
anniek-dev <am.edelaar@student.avans.nl>
2026-06-24 12:02:29 +0200

    fix

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -219,4 +219,28 @@ class EventController extends Controller
 
         return trim($normalized, '_');
     }
+
+    private function saveUploadedFunctionImage(Request $request)
+    {
+        $imageName = $request->file('image')->hashName();
+
+        $relativeFolder = 'images/events';
+        $imageFolder = public_path($relativeFolder);
+
+        if (!is_dir($imageFolder)) {
+            mkdir($imageFolder, 0777, true);
+        }
+
+        if (!is_writable($imageFolder)) {
+            chmod($imageFolder, 0777);
+        }
+
+        if (!is_writable($imageFolder)) {
+            abort(500, 'De map public/images/events is not writable. Move the project outside OneDrive or check the folder permissions.');
+        }
+
+        $request->file('image')->move($imageFolder, $imageName);
+
+        return $relativeFolder . '/' . $imageName;
+    }
 }
